b91ba37209f70f2a106eb8674247276cc750f5d5
abhijit20-vd <abhijitdarade.vd@gmail.com>
2023-06-19 06:17:13 +0530

    NewsMonkey

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const NewsItem = (props) => {
+
+
+    let {title, description, imageUrl, newsUrl, author, date, source} = props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div style={{display : 'flex', justifyContent : 'flex-end', position: 'absolute', right : 0}}>
+            <span
+              className="badge rounded-pill bg-danger"
+            >
+              {source}
+            </span>
+          </div>
+          <img
+            src={
+              !imageUrl
+                ? "https://fdn.gsmarena.com/imgroot/news/23/06/weekly-poll-realme-11pro-11proplus/-952x498w6/gsmarena_000.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+export default NewsItem
